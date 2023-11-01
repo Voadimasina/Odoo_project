bbd30cafd208cf639119e6c4f5fbfa56c87f1a84
Voadimasina <voadimasina@yahoo.com>
2023-11-01 02:00:05 +0100

    first commit

---
+++ custom/radiation_reporter/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2023 Voadimasina
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Radiation Reporter',
+    'description': """
+        Radiation reporter""",
+    'version': '16.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Voadimasina',
+    'website': 'www.odoo.com',
+    'depends': [
+        'contacts',
+    ],
+    'data': [
+        'views/ir_ui_menu.xml',
+        'views/res_partner.xml',
+    ],
+    'demo': [
+    ],
+}
